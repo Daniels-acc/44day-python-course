6beb64763fb554d3d869ba94fd16f2297f4f9ff6
daniel.gluhak <gluhakdaniel@gmail.com>
2023-01-25 11:23:44 +0100

    Improving input and output to .txt file

--- app1/new_todoList.py
+++ app1/new_todoList.py
@@ -19,23 +19,32 @@ while True:
             todo_input = str(input("Create todo item: ")) + "\n"
 
             file = open('src/todos.txt', 'r') # open todos.txt file - pens a file for reading, error if the file does not exist
-            todos = file.readlines() # reads opened file - list file type
+            todos = file.readlines() # need to have this variable to store the data
             file.close()
 
-            todos.append(todo_input) # append items to that opened file and keeps existing in todos variable
+            todos.append(todo_input) # variable to append to list
+
             file = open('src/todos.txt', 'w') # it will overwrite the existing file and start session of writing,
             # creates the file if it does not exist
             file.writelines(todos)
             file.close()
 
         case 'read':
-            readFile()
+            file = open('src/todos.txt', 'r')
+            todos = file.readlines() # ovo je kao da smo napisali todos = [lista neka]
+            file.close()
 
-        case 'update':
-            print('Choose an item to edit: ')
-            file = readFile()
+            new_todos = []
+
+            for item in todos:
+                new_item = item.strip("\n")
+                new_todos.append(new_item)
+
+            print(f"Ovo printa listu:  {todos}")
+            print("List of todos: ")
+            for index, item in enumerate(new_todos): #iterate through that todo list and prints it
+                row = f"{index + 1}. {item.title()}"
+                print(row)
 
 
-        case 'exit':
-            print('Exiting')
-            break
+
